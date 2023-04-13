916ba5b203954684e518f63fa1c696172db2b15f
Sheldon Fereira <76541981+sfereira@users.noreply.github.com>
2023-04-13 14:57:47 -0400

    Created Validate_Stack_Sequences.py
    - Created a function to return boolean value to check if the given list of integers can be popped in a sequence.
    - Returns 'True' if the sequence pop is possible orelse 'False'

---
+++ Validate_Stack_Sequences.py
@@ -0,0 +1,38 @@
+# Given two integer arrays 'pushed' and 'popped' each with distinct values, 
+# return 'true' if this could have been the result of a sequence of push and pop operations on an initially empty stack, 
+# or 'false' otherwise.
+
+class Solution(object):
+    def validateStackSequences(self, pushed, popped):
+        """
+        :type pushed: List[int]
+        :type popped: List[int]
+        :rtype: bool
+        """
+        stack_list=[]  # holding list
+        count = 0  # counter
+        
+        for num in pushed:
+            stack_list.append(num)
+
+            while stack_list and stack_list[-1] == popped[count]:
+                stack_list.pop()
+                # increment the counter
+                count +=1
+        
+        # return boolean
+        return count == len(popped)
+      
+# Case Study 1        
+s = Solution()
+pushed = [1,2,3,4,5]
+popped = [4,5,3,2,1]
+result = s.twoSum(pushed, popped)
+print(result) # expected result -> true
+      
+# Case Study 2        
+s = Solution()
+pushed = [1,2,3,4,5]
+popped = [4,3,5,1,2]
+result = s.twoSum(pushed, popped)
+print(result) # expected result -> false
